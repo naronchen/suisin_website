765883a9b3ae183c0ac0e83b9b8ccbe3379471c3
Naron <naron.chen@richmond.edu>
2023-01-08 02:28:50 -0500

    updated contact/added format&font

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -2,39 +2,11 @@ function Contact() {
   return (
     <div className="container contact">
       <h2 className="main-title text-center">CONTACT</h2>
-      <div className="col-md-12">
-        <div className="row">
-          <div className="col-md-4 mb-1">
-            <input name="name" placeholder="Name" className="contact-input" />
-          </div>
-
-          <div className="col-md-4 mb-1">
-            <input name="email" placeholder="Email" className="contact-input" />
-          </div>
-          <div className="col-md-4 mb-1">
-            <input
-              name="subject"
-              placeholder="Subject"
-              className="contact-input"
-            />
-          </div>
+        <div class="contact-main">
+          <a href="https://www.facebook.com/profile.php?id=100023455459789" target="_blank" rel="noreferrer"><i class="fa fa-facebook fa-2x"></i></a>
+          <a href="" target="_blank" rel="noreferrer"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="" target="_blank" rel="noreferrer"><i class="fab fa-discord fa-2x"></i></a>
         </div>
-      </div>
-      <br />
-      <div className="col-md-12">
-      <textarea
-            name="message"
-            placeholder="Message"
-            className="contact-textarea"
-          />
-      </div>
-
-      <br></br>
-      <div className="row">
-        <div className="col-md-12">
-          <input className="form-btn" type="submit" value="Send Message" />
-        </div>
-      </div>
     </div>
   );
 }
