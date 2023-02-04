689680a3621577593afef431d59276ea30d484b3
Naron <naron.chen@richmond.edu>
2023-02-04 01:58:29 -0500

    fixed url routing

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -6,7 +6,7 @@ function About() {
       <div className="container about">
         <div className="row">
           <div className="col-md-6 text-center">
-            <img alt="about" src="../img/suisin_logo.svg" className="img-fluid" />
+            <img alt="about" src={process.env.PUBLIC_URL+"/img/suisin_logo.svg"} className="img-fluid" />
           </div>
           <div className="col-md-6">
             <h2 className="main-title about-h2">ABOUT</h2>
